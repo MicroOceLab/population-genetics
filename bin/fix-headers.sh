25e3c734c527b3605d851eb95b1931543cfc958b
neotechz <marc.ayalde@gmail.com>
2025-04-04 16:07:22 +0800

    Modify fix headers shell script to convert all underscore to dashes

--- bin/fix-headers.sh
+++ bin/fix-headers.sh
@@ -3,12 +3,13 @@
 argv=("$@");
 
 cp "${argv[0]}" "${argv[1]}"
-sed -i -e 's/;/_/g' "${argv[1]}"
-sed -i -e 's/:/_/g' "${argv[1]}"
 sed -i -e 's/(//g' "${argv[1]}"
 sed -i -e 's/)//g' "${argv[1]}"
+sed -i -e 's/;/_/g' "${argv[1]}"
+sed -i -e 's/:/_/g' "${argv[1]}"
 sed -i -e 's/,/_/g' "${argv[1]}"
 sed -i -e 's/ /_/g' "${argv[1]}"
 sed -i -e "s/\'//g" "${argv[1]}"
 sed -i -e 's/\./_/g' "${argv[1]}"
+sed -i -e 's/_/-/g' "${argv[1]}"
 
